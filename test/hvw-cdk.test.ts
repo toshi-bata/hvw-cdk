d58735ebe5511ff948fe85b1a903c3a73234187a
Toshiaki Kawabata <toshiaki@techsoft3d.com>
2026-08-24 10:56:53 +0900

    Initial commit

---
+++ test/hvw-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HvwCdk from '../lib/hvw-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hvw-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HvwCdk.HvwCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
